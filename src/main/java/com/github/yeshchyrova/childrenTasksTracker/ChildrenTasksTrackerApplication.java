8a75b58067aaccb8dc0d61d490fae2515f135169
yeshchyrova <e.shchyrova@gmail.com>
2024-10-15 09:07:47 +0200

    Initialise an empty project

---
+++ src/main/java/com/github/yeshchyrova/childrenTasksTracker/ChildrenTasksTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.yeshchyrova.childrenTasksTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildrenTasksTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildrenTasksTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yeshchyrova/childrenTasksTracker/ChildrenTasksTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yeshchyrova.childrenTasksTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildrenTasksTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

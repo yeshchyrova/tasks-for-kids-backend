4ee8b941bf6560f5a5cb19623bacc9e5986bc0a4
yeshchyrova <e.shchyrova@gmail.com>
2025-02-22 07:42:44 +0100

    Fix bugs

--- src/main/java/com/github/yeshchyrova/taskstracker/config/PasswordConfig.java
+++ src/main/java/com/github/yeshchyrova/taskstracker/config/PasswordConfig.java
@@ -1,5 +1,6 @@
 package com.github.yeshchyrova.taskstracker.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class PasswordConfig {
 
+  @Value("${DATABASE_URL:NOT_SET}")
+  private String databaseUrl;
+
   @Bean
   public PasswordEncoder passwordEncoder() {
+    System.out.println("databaseUrl: " + databaseUrl);
     return new BCryptPasswordEncoder();
   }
 }

dbba70725bbb620626d5bb87c949d4e4179794c9
yeshchyrova <e.shchyrova@gmail.com>
2025-02-22 08:26:56 +0100

    Change cors settings

--- src/main/java/com/github/yeshchyrova/taskstracker/config/SecurityConfig.java
+++ src/main/java/com/github/yeshchyrova/taskstracker/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://tasks-for-kids-frontend.vercel.app"));
     configuration.setAllowedMethods(Arrays.asList(
             HttpMethod.GET.name(),
             HttpMethod.POST.name(),
